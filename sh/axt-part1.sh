646093d52cc178b0d8e2ee97e6e658a87e63c09a
YBS-GH <52429295+YBS-PC@users.noreply.github.com>
2025-01-25 01:01:50 +0300

    Update axt-part1.sh

--- sh/axt-part1.sh
+++ sh/axt-part1.sh
@@ -40,4 +40,4 @@ echo "src-git youtubeUnblock https://github.com/Waujito/youtubeUnblock.git;openw
 echo "src-git fancontrol https://github.com/JiaY-shi/fancontrol.git" >> feeds.conf.default
 ##echo "src-git fantastic https://github.com/fantastic-packages/packages.git" >> feeds.conf.default
 echo "src-git internetdetector https://github.com/gSpotx2f/luci-app-internet-detector.git" >> feeds.conf.default
-# echo "src-git luci-app-log-viewer https://github.com/gSpotx2f/luci-app-log.git" >> feeds.conf.default
+echo "src-git logviewer https://github.com/gSpotx2f/luci-app-log.git" >> feeds.conf.default
