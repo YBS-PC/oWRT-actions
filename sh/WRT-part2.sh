799073d51843d483f00490579f6e74790148a816
YBS-GH <52429295+YBS-PC@users.noreply.github.com>
2025-06-09 18:29:23 +0300

    Update WRT-part2.sh

--- sh/WRT-part2.sh
+++ sh/WRT-part2.sh
@@ -7,5 +7,7 @@
 ###rm -rf ./feeds/luci/applications/luci-app-homeproxy
 ###git clone -b master https://github.com/immortalwrt/homeproxy.git ./feeds/luci/applications/luci-app-homeproxy/
 mkdir ./package/luci-app-log-viewer
+mkdir ./package/zapret-openwrt
 git clone -b master https://github.com/gSpotx2f/luci-app-log.git ./package/luci-app-log-viewer/
+git clone -b master https://github.com/remittor/zapret-openwrt.git ./package/zapret-openwrt/
 ./scripts/feeds install -a
