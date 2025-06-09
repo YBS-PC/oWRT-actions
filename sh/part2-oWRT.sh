536455ef675f46541af03de9d5e43e9fec1f6630
YBS-GH <52429295+YBS-PC@users.noreply.github.com>
2025-06-09 13:58:00 +0300

    Update part2-oWRT.sh

--- sh/part2-oWRT.sh
+++ sh/part2-oWRT.sh
@@ -9,6 +9,8 @@
 #
 mkdir ./package/luci-app-homeproxy
 mkdir ./package/luci-app-log-viewer
+mkdir ./package/zapret-openwrt
 git clone -b master https://github.com/immortalwrt/homeproxy.git ./package/luci-app-homeproxy/
 git clone -b master https://github.com/gSpotx2f/luci-app-log.git ./package/luci-app-log-viewer/
+git clone -b master https://github.com/remittor/zapret-openwrt.git ./package/zapret-openwrt/
 ./scripts/feeds install -a
