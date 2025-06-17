201f9b0038116dc58b4047cda9695efb82b4d213
YBS-GH <52429295+YBS-PC@users.noreply.github.com>
2025-06-17 18:21:35 +0300

    Update WRT-part1.sh

--- sh/WRT-part1.sh
+++ sh/WRT-part1.sh
@@ -34,3 +34,4 @@ echo "src-git internetdetector https://github.com/gSpotx2f/luci-app-internet-det
 ##echo "src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main" >> feeds.conf.default
 ##echo "src-git luciappxray https://github.com/yichya/luci-app-xray.git" >> feeds.conf.default
 #echo "src-git argon https://github.com/jerrykuku/luci-theme-argon.git" >> feeds.conf.default
+echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> feeds.conf.default
