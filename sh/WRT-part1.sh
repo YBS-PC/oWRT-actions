12221bfc284667d941c7fbf077bd1a7817a60532
YBS-GH <52429295+YBS-PC@users.noreply.github.com>
2025-06-17 23:44:38 +0300

    Update WRT-part1.sh

--- sh/WRT-part1.sh
+++ sh/WRT-part1.sh
@@ -31,7 +31,7 @@ echo "src-git internetdetector https://github.com/gSpotx2f/luci-app-internet-det
 #echo "src-git fantastic https://github.com/fantastic-packages/fantastic-packages-feeds.git" >> feeds.conf.default
 #echo "src-git logviewer https://github.com/fantastic-packages/packages.git;feeds/luci/luci-app-log-viewer" >> feeds.conf.default
 #echo "src-git logviewer https://github.com/gSpotx2f/luci-app-log.git" >> feeds.conf.default
-##echo "src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main" >> feeds.conf.default
-##echo "src-git luciappxray https://github.com/yichya/luci-app-xray.git" >> feeds.conf.default
+#echo "src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main" >> feeds.conf.default
+#echo "src-git luciappxray https://github.com/yichya/luci-app-xray.git" >> feeds.conf.default
 #echo "src-git argon https://github.com/jerrykuku/luci-theme-argon.git" >> feeds.conf.default
-echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> feeds.conf.default
+#echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> feeds.conf.default
