316bc593db4907fbc08734e31dad1211d1aa8eba
YBS-GH <52429295+YBS-PC@users.noreply.github.com>
2025-07-11 21:28:22 +0300

    Update WRT-part2.sh

--- sh/WRT-part2.sh
+++ sh/WRT-part2.sh
@@ -8,6 +8,7 @@ git clone -b main https://github.com/openwrt-xiaomi/facinstall.git ./package/fac
 git clone -b master https://github.com/gSpotx2f/luci-app-log.git ./package/luci-app-log-viewer/
 git clone -b master https://github.com/remittor/zapret-openwrt.git ./package/zapret-openwrt/
 ./scripts/feeds install -a
+
 #
 #sed -i 's/192.168.1.1/10.10.0.1/g' ./package/base-files/files/bin/config_generate
 #mkdir ./package/custom
