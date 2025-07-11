0da13187a32143d33ce96c49929eecf55a4e8849
YBS-GH <52429295+YBS-PC@users.noreply.github.com>
2025-07-11 21:27:35 +0300

    Update WRT-part2.sh

--- sh/WRT-part2.sh
+++ sh/WRT-part2.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-#sed -i 's/192.168.1.1/10.10.0.1/g' ./package/base-files/files/bin/config_generate
-#mkdir ./package/custom
-#git clone https://github.com/sbwml/autocore-arm.git ./package/custom/
-#./scripts/feeds install -a
-###rm -rf ./feeds/luci/applications/luci-app-homeproxy
-###git clone -b master https://github.com/immortalwrt/homeproxy.git ./feeds/luci/applications/luci-app-homeproxy/
+#
 mkdir ./package/luci-app-log-viewer
 mkdir ./package/zapret-openwrt
 mkdir ./package/facinstall
