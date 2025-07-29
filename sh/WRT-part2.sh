33bb798f8d0ae76764a1d576496b7a36a9c0702a
YBS-GH <52429295+YBS-PC@users.noreply.github.com>
2025-07-29 21:34:58 +0300

    Update WRT-part2.sh

--- sh/WRT-part2.sh
+++ sh/WRT-part2.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 #
-echo ">>> Force-disabling unwanted packages..."
+##echo ">>> Force-disabling unwanted packages..."
 # Список пакетов, которые нужно принудительно отключить.
 # apk-mbedtls libmbedtls libwolfssl libustream-mbedtls wpad-basic-mbedtls mbedtls-util
 # autocore automount block-mount bridger cpufreq luci-app-cpufreq default-settings-chn 
-BLACKLIST=(
-    bind
-    wolfssl
-    zabbix
-    mbedtls
-    autocore
-    block-mount
-    bridger
-    default-settings-chn
-    cpufreq
-    automount
-)
+##BLACKLIST=(
+##    bind
+##    wolfssl
+##    zabbix
+##    mbedtls
+##    autocore
+##    block-mount
+##    bridger
+##    default-settings-chn
+##    cpufreq
+##    automount
+##)
 
-for pkg in "${BLACKLIST[@]}"; do
-    # Удаляем все существующие строки для этого пакета (с любым значением) sed -i -e "/CONFIG_PACKAGE_.*${pkg}/Id" -e "/CONFIG_.*${pkg}/Id" ./.config
-    sed -i "/CONFIG_PACKAGE_.*${pkg}/Id" ./.config
-    sed -i "/CONFIG_.*${pkg}/Id" ./.config
-    # Добавляем новую строку, которая явно отключает пакет
-    echo "# CONFIG_PACKAGE_${pkg} is not set" >> ./.config
-done
+##for pkg in "${BLACKLIST[@]}"; do
+##    # Удаляем все существующие строки для этого пакета (с любым значением) sed -i -e "/CONFIG_PACKAGE_.*${pkg}/Id" -e "/CONFIG_.*${pkg}/Id" ./.config
+##    sed -i "/CONFIG_PACKAGE_.*${pkg}/Id" ./.config
+##    sed -i "/CONFIG_.*${pkg}/Id" ./.config
+##    # Добавляем новую строку, которая явно отключает пакет
+##    echo "# CONFIG_PACKAGE_${pkg} is not set" >> ./.config
+##done
 #
 mkdir ./package/luci-app-log-viewer
 mkdir ./package/zapret-openwrt
