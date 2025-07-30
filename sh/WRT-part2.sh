fb56fea58a98b900d7bc2ad58b00ef9347d6fe03
YBS-GH <52429295+YBS-PC@users.noreply.github.com>
2025-07-30 10:12:57 +0300

    Update WRT-part2.sh

--- sh/WRT-part2.sh
+++ sh/WRT-part2.sh
@@ -19,10 +19,9 @@
 ##
 ##for pkg in "${BLACKLIST[@]}"; do
 ##    # Удаляем все существующие строки для этого пакета (с любым значением) sed -i -e "/CONFIG_PACKAGE_.*${pkg}/Id" -e "/CONFIG_.*${pkg}/Id" ./.config
-##    sed -i "/CONFIG_PACKAGE_.*${pkg}/Id" ./.config
 ##    sed -i "/CONFIG_.*${pkg}/Id" ./.config
 ##    # Добавляем новую строку, которая явно отключает пакет
-##    echo "# CONFIG_PACKAGE_${pkg} is not set" >> ./.config
+##    echo "CONFIG_PACKAGE_${pkg}=n" >> ./.config
 ##done
 #
 mkdir ./package/luci-app-log-viewer
