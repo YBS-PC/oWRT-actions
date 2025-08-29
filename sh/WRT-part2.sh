e4dd9dbe2f40b8e720a786acc2dcee369267ba37
YBS-GH <52429295+YBS-PC@users.noreply.github.com>
2025-08-29 23:01:02 +0300

    Update WRT-part2.sh

--- sh/WRT-part2.sh
+++ sh/WRT-part2.sh
@@ -39,7 +39,9 @@ git clone -b master https://github.com/remittor/zapret-openwrt.git ./package/zap
 ./scripts/feeds install -a
 
 #
-#sed -i 's/192.168.1.1/10.10.0.1/g' ./package/base-files/files/bin/config_generate
+sed -i "s/192.168.1.1/192.168.2.1/g" ./package/base-files/files/bin/config_generate
+sed -i "s#zonename='UTC'#zonename='Europe/Moscow'#g" ./package/base-files/files/bin/config_generate
+sed -i "s#timezone='GMT0'#timezone='MSK-3'#g" ./package/base-files/files/bin/config_generate
 #mkdir ./package/custom
 #git clone https://github.com/sbwml/autocore-arm.git ./package/custom/
 #./scripts/feeds install -a
