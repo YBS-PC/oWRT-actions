5fbc7852aab3352728f9fc512624c668d6803ade
YBS-GH <52429295+YBS-PC@users.noreply.github.com>
2026-07-16 16:38:58 +0300

    Update zz1-final-offline-setup.sh

--- files/etc/uci-defaults/zz1-final-offline-setup.sh
+++ files/etc/uci-defaults/zz1-final-offline-setup.sh
@@ -494,6 +494,7 @@ if command -v hwclock >/dev/null 2>&1; then
     run_cmd "Синхронизация RTC" hwclock -s -u
 fi
 
+date "+%Y-%m-%d %H:%M:%S"
 if ping -c 1 -W 1 8.8.8.8 >/dev/null 2>&1; then
     log_info "Интернет есть, принудительная NTP синхронизация"
     /etc/init.d/sysntpd stop 2>/dev/null
@@ -502,7 +503,10 @@ if ping -c 1 -W 1 8.8.8.8 >/dev/null 2>&1; then
     hwclock -w -u 2>/dev/null && log_ok "RTC обновлён"
 else
     /etc/init.d/sysntpd restart 2>/dev/null
+	log_info "Пинг 8.8.8.8 пока не доступен"
 fi
+date "+%Y-%m-%d %H:%M:%S"
+
 sleep 2
 
 cp /etc/banner /etc/banner.bak
